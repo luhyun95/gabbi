88f77e6a84743e420fe21e56a0d89dbeb6388b0d
luhyun95 <cathykimhj@gmail.com>
2023-11-20 11:40:49 +0800

    .

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const Sentiment = require('sentiment');
 const app = express();
 const port = 3000;
 
@@ -8,22 +9,59 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-// Replace 'your_api_key' with your actual OpenAI API key
+// OpenAI API key
 const OPENAI_API_KEY = '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';
 
-//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
+// Sentiment analysis instance
+const sentiment = new Sentiment();
 
+const questions = [
+    "What activity makes you feel most like yourself?",
+    "Describe a dream you've had more than once.",
+    "What's a skill you've always wanted to learn?",
+    "When do you feel most at peace?",
+    // ... more questions ...
+];
+
+// Function to analyze sentiment
+function analyzeSentiment(answer) {
+    const result = sentiment.analyze(answer);
+    return result.score; // Returns a score between -5 and 5
+}
+
+// Function to determine the category based on your logic
+function determineCategory(answer) {
+    const sentimentScore = analyzeSentiment(answer);
+    const answerLength = answer.split(' ').length;
+    const keywords = {/* ... same as before ... */};
+    const elementScores = { Fire: 0, Earth: 0, Water: 0, Air: 0 };
+
+    // Convert answer to lower case for case-insensitive matching
+    const lowerCaseAnswer = answer.toLowerCase();
+
+    // Update scores based on keywords
+    for (const element in keywords) {
+        keywords[element].forEach(keyword => {
+            if (lowerCaseAnswer.includes(keyword)) {
+                elementScores[element]++;
+            }
+        });
+    }
+
+    // ... rest of the logic remains the same ...
+
+    return maxElement || 'Undetermined';
+}
+
+// Endpoint to analyze user answer
 app.post('/analyze', async (req, res) => {
     const userAnswer = req.body.answer;
-    // Logic to categorize the answer goes here
-    // For example, you might check for certain keywords or themes
+    const category = determineCategory(userAnswer);
 
-    let category = determineCategory(userAnswer); // Implement this function based on your logic
-
-    // Now, make a request to OpenAI API
+    // OpenAI API request
     try {
         const response = await axios.post('https://api.openai.com/v1/engines/davinci/completions', {
-            prompt: "Generate a response based on the category: " + category,
+            prompt: `Write a response in a [Tone: Encouraging] for a person with [Category: ${category}]. The user's answer was: [${userAnswer}]`,
             max_tokens: 150
         }, {
             headers: {
@@ -36,6 +74,13 @@ app.post('/analyze', async (req, res) => {
         console.error(error);
         res.status(500).send('Error processing the request');
     }
+});
+
+// Endpoint to get a random question
+app.get('/get-random-question', (req, res) => {
+    const randomIndex = Math.floor(Math.random() * questions.length);
+    const randomQuestion = questions[randomIndex];
+    res.json({ question: randomQuestion });
 });
 
 app.listen(port, () => {
